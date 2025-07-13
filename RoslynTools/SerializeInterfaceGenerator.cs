f615f21264a8b08078158c4a313b5f60eb185989
Alexander Larsen <a.lrsn@outlook.com>
2025-07-13 09:18:15 +0200

    Refactor Roslyn source generators structure
    - Changed `SourceGenerators` folder and csproj to `RoslynTools` and updated namespaces for clarity.
    - Moved DLLs to `RoslynLibs` directory.
    - Adjusted `.gitignore` to reflect the changes.

--- RoslynTools/SerializeInterfaceGenerator.cs
+++ RoslynTools/SerializeInterfaceGenerator.cs
@@ -5,7 +5,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
-namespace SerializedDIGenerators;
+namespace Saneject.Roslyn.Generators;
 
 [Generator]
 public class SerializeInterfaceGenerator : ISourceGenerator
