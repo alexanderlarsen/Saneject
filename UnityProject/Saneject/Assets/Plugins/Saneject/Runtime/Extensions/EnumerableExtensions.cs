50a46fd52e5ac207765aaa49d6d3de90abff9440
Alexander Larsen <a.lrsn@outlook.com>
2025-07-30 07:31:57 +0200

    Rename `ObjectExtensions` to `EnumerableExtensions` and remove Unity dependency.

---
+++ UnityProject/Saneject/Assets/Plugins/Saneject/Runtime/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Plugins.Saneject.Runtime.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Wrap the <paramref name="item" /> in an <see cref="IEnumerable{T}" /> (empty if null).
+        /// </summary>
+        public static IEnumerable<T> WrapInEnumerable<T>(this T item)
+        {
+            return item != null ? new[] { item } : Enumerable.Empty<T>();
+        }
+    }
+}
