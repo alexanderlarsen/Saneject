750a7214c643110633ab7f59bc34825405daeed0
Alexander Larsen <alexanderlarsen@users.noreply.github.com>
2025-10-24 09:25:41 +0200

    Add tests and classes for nested serialization and targeted injection

---
+++ UnityProject/Saneject/Assets/Tests/Runtime/NestedSerializable/DeepNestedClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Plugins.Saneject.Runtime.Attributes;
+using Tests.Runtime.Component;
+using UnityEngine;
+
+namespace Tests.Runtime.NestedSerializable
+{
+    [Serializable]
+    public class DeepNestedClass
+    {
+        [Inject, SerializeField]
+        private InjectableComponent injectableComponent;
+    }
+}
---
+++ UnityProject/Saneject/Assets/Tests/Runtime/NestedSerializable/NestedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Plugins.Saneject.Runtime.Attributes;
+using Tests.Runtime.Component;
+using UnityEngine;
+
+namespace Tests.Runtime.NestedSerializable
+{
+    [Serializable]
+    public class NestedClass
+    {
+        [Inject, SerializeField]
+        private InjectableComponent injectableComponent;
+
+        [SerializeField]
+        private DeepNestedClass deepNestedClass;
+    }
+}
