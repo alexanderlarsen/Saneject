6e8d78b8d332d54ff1a80a1dbf97f8d965bf3c7f
Alexander Larsen <alexanderlarsen@users.noreply.github.com>
2026-03-20 11:44:11 +0100

    Hide `ComponentMenuPriority` class from public API

--- UnityProject/Saneject/Assets/Plugins/Saneject/Editor/Menus/ComponentMenuItems/ComponentMenuPriority.cs
+++ UnityProject/Saneject/Assets/Plugins/Saneject/Editor/Menus/ComponentMenuItems/ComponentMenuPriority.cs
@@ -1,9 +1,13 @@
-﻿namespace Plugins.Saneject.Editor.Menus.ComponentMenuItems
+﻿using System.ComponentModel;
+
+namespace Plugins.Saneject.Editor.Menus.ComponentMenuItems
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public class ComponentMenuPriority
     {
         public const int Root = -10000;
 
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public static class Filter
         {
             public const int Base = Root + 0;
