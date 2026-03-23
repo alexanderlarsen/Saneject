c2509af2b393d349b5b2f727177252f1cab8a2e7
Alexander Larsen <alexanderlarsen@users.noreply.github.com>
2026-03-23 20:25:05 +0100

    Add special locator binding tests for `ComponentLocators`

---
+++ UnityProject/Saneject/Assets/Tests/Saneject/Editor/Binding/Locators/ComponentLocators/SpecialTests.cs
@@ -0,0 +1,75 @@
+using NUnit.Framework;
+using Plugins.Saneject.Editor.Data.Context;
+using Plugins.Saneject.Editor.Pipeline;
+using Tests.Saneject.Editor.Utilities;
+using Tests.Saneject.Fixtures.Scripts;
+
+namespace Tests.Saneject.Editor.Binding.Locators.ComponentLocators
+{
+    public class SpecialTests
+    {
+        [Test]
+        public void FromAnywhere_InjectsFromAnywhereInScene()
+        {
+            TestScene scene = TestScene.Create(roots: 2, width: 3, depth: 3);
+            scene.AddDependencyAt("Root 2/Child 3/Child 3");
+
+            TestScope scope = scene.AddComponent<TestScope>("Root 1");
+            ConcreteTarget target = scene.AddComponent<ConcreteTarget>("Root 1/Child 1");
+            Dependency expected = scene.GetComponent<Dependency>("Root 2/Child 3/Child 3");
+
+            scope.BindComponent<Dependency>().FromAnywhere();
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+            Assert.AreEqual(expected, target.dependency);
+        }
+
+        [Test]
+        public void FromInstance_InjectsSpecifiedInstance()
+        {
+            TestScene scene = TestScene.Create(roots: 2, width: 3, depth: 3);
+            scene.AddDependencyAt("Root 2/Child 3/Child 3");
+
+            TestScope scope = scene.AddComponent<TestScope>("Root 1");
+            ConcreteTarget target = scene.AddComponent<ConcreteTarget>("Root 1/Child 1");
+            Dependency expected = scene.GetComponent<Dependency>("Root 2/Child 3/Child 3");
+
+            scope.BindComponent<Dependency>().FromInstance(expected);
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+            Assert.AreEqual(expected, target.dependency);
+        }
+
+        [Test]
+        public void FromMethod_InjectsComponentReturnedByMethod()
+        {
+            TestScene scene = TestScene.Create(roots: 2, width: 3, depth: 3);
+            scene.AddDependencyAt("Root 2/Child 3/Child 3");
+
+            TestScope scope = scene.AddComponent<TestScope>("Root 1");
+            ConcreteTarget target = scene.AddComponent<ConcreteTarget>("Root 1/Child 1");
+            Dependency expected = scene.GetComponent<Dependency>("Root 2/Child 3/Child 3");
+
+            scope.BindComponent<Dependency>().FromMethod(() => scene.GetComponent<Dependency>("Root 2/Child 3/Child 3"));
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+            Assert.AreEqual(expected, target.dependency);
+        }
+
+        [Test]
+        public void FromMethodEnumerable_InjectsSingleComponentReturnedByMethod()
+        {
+            TestScene scene = TestScene.Create(roots: 2, width: 3, depth: 3);
+            scene.AddDependencyAt("Root 2/Child 3/Child 3");
+
+            TestScope scope = scene.AddComponent<TestScope>("Root 1");
+            ConcreteTarget target = scene.AddComponent<ConcreteTarget>("Root 1/Child 1");
+            Dependency expected = scene.GetComponent<Dependency>("Root 2/Child 3/Child 3");
+
+            scope.BindComponent<Dependency>().FromMethod(() => new[] { scene.GetComponent<Dependency>("Root 2/Child 3/Child 3") });
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+            Assert.AreEqual(expected, target.dependency);
+        }
+    }
+}
