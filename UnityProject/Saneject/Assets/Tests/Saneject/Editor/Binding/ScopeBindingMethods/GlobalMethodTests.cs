41cbf29746c72756ea25f93551dc5de7bc2f22ef
Alexander Larsen <alexanderlarsen@users.noreply.github.com>
2026-03-24 11:18:44 +0100

    Add GlobalMethodTests to verify global scope binding

---
+++ UnityProject/Saneject/Assets/Tests/Saneject/Editor/Binding/ScopeBindingMethods/GlobalMethodTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework;
+using Plugins.Saneject.Editor.Data.Context;
+using Plugins.Saneject.Editor.Pipeline;
+using Plugins.Saneject.Runtime.Scopes;
+using Tests.Saneject.Fixtures.Scripts;
+using Tests.Saneject.Fixtures.Scripts.Dependencies;
+using UnityEngine;
+
+namespace Tests.Saneject.Editor.Binding.ScopeBindingMethods
+{
+    public class GlobalMethodTests
+    {
+        [Test]
+        public void BindGlobal_TConcrete_AddsTConcreteToScopeGlobalComponents()
+        {
+            TestScene scene = TestScene.Create(roots: 1, width: 1, depth: 1);
+            TestScope scope = scene.Add<TestScope>("Root 1");
+            ComponentDependency dependency = scene.Add<ComponentDependency>("Root 1");
+            List<Component> globalComponentsList = GetScopeGlobalComponentsList(scope);
+
+            scope.BindGlobal<ComponentDependency>().FromSelf();
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+
+            Assert.That(dependency, Is.Not.Null);
+            CollectionAssert.Contains(globalComponentsList, dependency);
+        }
+
+        private static List<Component> GetScopeGlobalComponentsList(Scope scope)
+        {
+            FieldInfo fieldInfo = typeof(Scope).GetField("globalComponents", BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.That(fieldInfo, Is.Not.Null);
+            return fieldInfo.GetValue(scope) as List<Component>;
+        }
+    }
+}
