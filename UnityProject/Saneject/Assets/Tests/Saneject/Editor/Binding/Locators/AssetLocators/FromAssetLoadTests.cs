249205690421c4ad0c76369ef99e47500b0b1080
Alexander Larsen <alexanderlarsen@users.noreply.github.com>
2026-03-24 08:41:36 +0100

    Add asset binding tests for additional locator scenarios

--- UnityProject/Saneject/Assets/Tests/Saneject/Editor/Binding/Locators/AssetLocators/FromAssetLoadTests.cs
+++ UnityProject/Saneject/Assets/Tests/Saneject/Editor/Binding/Locators/AssetLocators/FromAssetLoadTests.cs
@@ -1,7 +1,47 @@
-﻿namespace Tests.Saneject.Editor.Binding.Locators.AssetLocators
+﻿using NUnit.Framework;
+using Plugins.Saneject.Editor.Data.Context;
+using Plugins.Saneject.Editor.Pipeline;
+using Tests.Saneject.Fixtures.Scripts;
+using UnityEngine;
+
+namespace Tests.Saneject.Editor.Binding.Locators.AssetLocators
 {
     public class FromAssetLoadTests
     {
+        [Test]
+        public void FromAssetLoad_InjectsAsset()
+        {
+            TestScene scene = TestScene.Create(roots: 1, width: 1, depth: 1);
+
+            TestScope scope = scene.Add<TestScope>("Root 1");
+            ConcreteAssetTarget target = scene.Add<ConcreteAssetTarget>("Root 1");
+            AssetDependency expected = Resources.Load<AssetDependency>("AssetDependency 1");
+
+            scope.BindAsset<AssetDependency>().FromAssetLoad("Assets/Tests/Saneject/Fixtures/Resources/AssetDependency 1.asset");
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+
+            Assert.NotNull(expected);
+            Assert.NotNull(target.dependency);
+            Assert.AreEqual(expected, target.dependency);
+        }
         
+        [Test]
+        public void FromAssetLoadAll_InjectsAsset()
+        {
+            TestScene scene = TestScene.Create(roots: 1, width: 1, depth: 1);
+
+            TestScope scope = scene.Add<TestScope>("Root 1");
+            ConcreteAssetTarget target = scene.Add<ConcreteAssetTarget>("Root 1");
+            AssetDependency expected = Resources.Load<AssetDependency>("AssetDependency 1");
+
+            scope.BindAsset<AssetDependency>().FromAssetLoadAll("Assets/Tests/Saneject/Fixtures/Resources/AssetDependency 1.asset");
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+
+            Assert.NotNull(expected);
+            Assert.NotNull(target.dependency);
+            Assert.AreEqual(expected, target.dependency);
+        }
     }
 }
--- UnityProject/Saneject/Assets/Tests/Saneject/Editor/Binding/Locators/AssetLocators/SpecialTests.cs
+++ UnityProject/Saneject/Assets/Tests/Saneject/Editor/Binding/Locators/AssetLocators/SpecialTests.cs
@@ -1,7 +1,68 @@
-﻿namespace Tests.Saneject.Editor.Binding.Locators.AssetLocators
+﻿using NUnit.Framework;
+using Plugins.Saneject.Editor.Data.Context;
+using Plugins.Saneject.Editor.Pipeline;
+using Tests.Saneject.Fixtures.Scripts;
+using UnityEngine;
+
+namespace Tests.Saneject.Editor.Binding.Locators.AssetLocators
 {
     public class SpecialTests
     {
-        
+        [Test]
+        public void FromInstance_InjectsAsset()
+        {
+            TestScene scene = TestScene.Create(roots: 1, width: 1, depth: 1);
+
+            TestScope scope = scene.Add<TestScope>("Root 1");
+            ConcreteAssetTarget target = scene.Add<ConcreteAssetTarget>("Root 1");
+            AssetDependency expected = Resources.Load<AssetDependency>("AssetDependency 1");
+
+            scope.BindAsset<AssetDependency>().FromInstance(expected);
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+
+            Assert.NotNull(expected);
+            Assert.NotNull(target.dependency);
+            Assert.AreEqual(expected, target.dependency);
+        }
+
+        [Test]
+        public void FromMethod_InjectsAsset()
+        {
+            TestScene scene = TestScene.Create(roots: 1, width: 1, depth: 1);
+
+            TestScope scope = scene.Add<TestScope>("Root 1");
+            ConcreteAssetTarget target = scene.Add<ConcreteAssetTarget>("Root 1");
+            AssetDependency expected = Resources.Load<AssetDependency>("AssetDependency 1");
+
+            scope.BindAsset<AssetDependency>().FromMethod(() => expected);
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+
+            Assert.NotNull(expected);
+            Assert.NotNull(target.dependency);
+            Assert.AreEqual(expected, target.dependency);
+        }
+
+        [Test]
+        public void FromMethodEnumerable_InjectsAsset()
+        {
+            TestScene scene = TestScene.Create(roots: 1, width: 1, depth: 1);
+
+            TestScope scope = scene.Add<TestScope>("Root 1");
+            ConcreteAssetTarget target = scene.Add<ConcreteAssetTarget>("Root 1");
+            AssetDependency expected = Resources.Load<AssetDependency>("AssetDependency 1");
+
+            scope.BindAsset<AssetDependency>().FromMethod(() => new[]
+            {
+                expected
+            });
+
+            InjectionRunner.Run(scene.Roots, ContextWalkFilter.SceneObjects);
+
+            Assert.NotNull(expected);
+            Assert.NotNull(target.dependency);
+            Assert.AreEqual(expected, target.dependency);
+        }
     }
 }
