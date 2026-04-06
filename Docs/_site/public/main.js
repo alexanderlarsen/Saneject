9f1ce5d35381e17cadd9c874493350658a7d2182
Alexander Larsen <alexanderlarsen@users.noreply.github.com>
2026-04-06 09:48:11 +0200

    Remove docs output from .gitignore

---
+++ Docs/_site/public/main.js
@@ -0,0 +1,14 @@
+﻿export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/alexanderlarsen/Saneject',
+            title: 'Saneject GitHub repo'
+        },
+        {
+            icon: 'file-earmark-text',
+            href: 'https://github.com/alexanderlarsen/Saneject/blob/main/LICENSE',
+            title: 'MIT license'
+        }
+    ]
+}
